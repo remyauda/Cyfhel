02a8d55c6df853a2a8f43f7c38c78d1504f24900
remyauda <remyauda@yahoo.fr>
2017-12-17 01:29:11 +0100

    [UPDATE] Update import

--- src/Cyfhel.cpp
+++ src/Cyfhel.cpp
@@ -1,16 +1,4 @@
-#include <NTL/ZZ.h>
-#include <NTL/ZZX.h>
-#include <NTL/BasicThreadPool.h>
-#include <NTL/lzz_pXFactoring.h>
-#include <cassert>
-#include <cstdio>
-#include <fstream>
-#include <unistd.h>
-#include <iostream>
-#include <cstddef>
-#include <sys/time.h>
+#include "Cyfhel.h"
 
-#include <FHE.h>
-#include <timing.h>
-#include <EncryptedArray.h>
-#include "Afhel.h"
+using namespace std;
+
--- src/Cyfhel.h
+++ src/Cyfhel.h
@@ -1,6 +1,9 @@
 #ifndef DEF_CYFHEL
 #define DEF_CYFHEL
 
+#include <unistd.h>
+#include <iostream>
+#include <cstddef>
 #include <fstream>
 #include <sstream>
 #include <cstdlib>
@@ -9,6 +12,15 @@
 
 #include <boost/unordered_map.hpp>
 #include <boost/lexical_cast.hpp>
+
+#include <NTL/ZZ.h>
+#include <NTL/ZZX.h>
+#include <NTL/BasicThreadPool.h>
+#include <NTL/lzz_pXFactoring.h>
+#include <cassert>
+#include <cstdio>
+
+#include <timing.h>
 
 #include "FHE.h"
 #include "EncryptedArray.h"

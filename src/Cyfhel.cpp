eb9e41d6bd13e71913421eeb084ef9b5afc9eb59
remyauda <remyauda@yahoo.fr>
2017-12-17 01:08:03 +0100

    [ADD] Add Helib + Minimal code for Cyfhel

---
+++ src/Cyfhel.cpp
@@ -0,0 +1,16 @@
+#include <NTL/ZZ.h>
+#include <NTL/ZZX.h>
+#include <NTL/BasicThreadPool.h>
+#include <NTL/lzz_pXFactoring.h>
+#include <cassert>
+#include <cstdio>
+#include <fstream>
+#include <unistd.h>
+#include <iostream>
+#include <cstddef>
+#include <sys/time.h>
+
+#include <FHE.h>
+#include <timing.h>
+#include <EncryptedArray.h>
+#include "Afhel.h"
---
+++ src/Cyfhel.h
@@ -0,0 +1,45 @@
+#ifndef DEF_CYFHEL
+#define DEF_CYFHEL
+
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <sys/time.h>
+#include <string.h>
+
+#include <boost/unordered_map.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include "FHE.h"
+#include "EncryptedArray.h"
+#include "PAlgebra.h"
+
+//La Class Cyfhel
+class Cyfhel {
+
+private:
+
+    /******ATTRIBUTES******/
+
+
+
+public:
+
+    /******CONSTRUCTOR WITH PARAMETERS******/
+
+
+
+    /******GETTERS******/
+
+
+
+    /******SETTERS******/
+
+
+
+    /******PROTOTYPES OF METHODS******/
+
+
+};
+
+#endif

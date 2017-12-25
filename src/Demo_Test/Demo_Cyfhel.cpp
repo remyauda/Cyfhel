20650f853c2358a01301e59b3028a68207228fbc
AUDA <remyauda@yahoo.fr>
2017-12-25 14:12:36 +0100

    [UPDATE] Change name of Cyfhel variable from he to cy.

--- src/Demo_Test/Demo_Cyfhel.cpp
+++ src/Demo_Test/Demo_Cyfhel.cpp
@@ -15,7 +15,7 @@
 int main()
 {
 //    string fileName = "DemoCyfhelEnv";
-    Cyfhel he = new Cyfhel(true);// Create object Cyfhel and enable print for all functions  
+    Cyfhel cy = new Cyfhel(true);// Create object Cyfhel and enable print for all functions  
     // Values for the modulus p (size of p):
     //   - 2 (Binary)
     //   - 257 (Byte)
@@ -31,10 +31,10 @@ int main()
 
     Timer timerDemo = new Timer(true);
     timerDemo.start();
-    he.keyGen(p, r, c, d, sec, w, L);
+    cy.keyGen(p, r, c, d, sec, w, L);
     vector<long> v1;
     vector<long> v2;
-    for(int i=0; i<he.nslots; i++){
+    for(int i=0; i<cy.nslots; i++){
         if(i<VECTOR_SIZE){
 		v1.push_back(i);
 	}
@@ -42,7 +42,7 @@ int main()
 		v1.push_back(0);
 	}
     }
-    for(int i=0; i<he.nslots; i++){
+    for(int i=0; i<cy.nslots; i++){
         if(i<VECTOR_SIZE){
 		v2.push_back(2);
 	}
@@ -52,36 +52,36 @@ int main()
     }
 
     // Sum
-    string k1 = he.encrypt(v1);
-    string k2 = he.encrypt(v2);
-    he.add(k1, k2);
-    vector<long> vRes = he.decrypt(k1);
+    string k1 = cy.encrypt(v1);
+    string k2 = cy.encrypt(v2);
+    cy.add(k1, k2);
+    vector<long> vRes = cy.decrypt(k1);
  
 
     // Multiplication
-    k1 = he.encrypt(v1);
-    k2 = he.encrypt(v2);
-    he.mult(k1, k2);
-    vector<long> vRes2 = he.decrypt(k1);
+    k1 = cy.encrypt(v1);
+    k2 = cy.encrypt(v2);
+    cy.mult(k1, k2);
+    vector<long> vRes2 = cy.decrypt(k1);
 
     // Scalar product
-    k1 = he.encrypt(v1);
-    k2 = he.encrypt(v2);
-    he.scalarProd(k1, k2);
-    vector<long> vRes3 = he.decrypt(k1);
+    k1 = cy.encrypt(v1);
+    k2 = cy.encrypt(v2);
+    cy.scalarProd(k1, k2);
+    vector<long> vRes3 = cy.decrypt(k1);
 
     // Square
-    k1 = he.encrypt(v1);
-    he.square(k1);
-    vector<long> vRes4 = he.decrypt(k1);
+    k1 = cy.encrypt(v1);
+    cy.square(k1);
+    vector<long> vRes4 = cy.decrypt(k1);
 
     timerDemo.stop();
     timerDemo.benchmarkInSeconds();
     timerDemo.benchmarkInHoursMinutesSecondsMillisecondes(true);
 
     // Store & retrieve environment
-//    he.saveEnv(fileName);
-    std::cout << "Saved env with values: m=" << he.getM() <<", p=" << he.getP() << ", r=" << he.getR() << endl;
+//    cy.saveEnv(fileName);
+    std::cout << "Saved env with values: m=" << cy.getM() <<", p=" << cy.getP() << ", r=" << cy.getR() << endl;
     std::cout << "END OF DEMO" << endl;
 };
 

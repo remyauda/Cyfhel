45cc29cbb4e0a943a5aff9d7ca326306d94f39de
remyauda <remyauda@yahoo.fr>
2018-01-01 23:27:08 +0100

    [ADD/UPDATE] Add CyCtxt in HElib + Update Makefile HElib.

---
+++ HElib/src/CyCtxt.cpp
@@ -0,0 +1,74 @@
+/*
+ * CyCtxt
+ * --------------------------------------------------------------------
+ *  Cyfhel is a C++ library that creates an abstraction over the basic
+ *  functionalities of HElib as a Homomorphic Encryption library, such as
+ *  addition, multiplication, scalar product and others.
+ *
+ *  Cyfhel implements a higher level of abstraction than HElib, and handles
+ *  Cyphertexts using an unordered map (key-value pairs) that is accessed
+ *  via keys of type string. This is done in order to manage Cyphertext
+ *  using references (the keys), which will allow Cyfhel to work only
+ *  using strings (keeping the Cyphertexts in C++). Cyfhel also compresses
+ *  the Context setup and Key generation into one single KeyGen function
+ *  with multiple parameter selection.
+ *  --------------------------------------------------------------------
+ *  Author: Remy AUDA
+ *  Date: 17/12/2017  
+ *  --------------------------------------------------------------------
+ *  License: GNU GPL v3
+ *  
+ *  Cyfhel is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Cyfhel is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  --------------------------------------------------------------------
+ */
+
+#include "CyCtxt.h"
+
+using namespace std;
+
+/******CONSTRUCTOR BY DEFAULT******/
+
+
+/******CONSTRUCTOR WITH PARAMETERS******/
+
+
+/******DESTRUCTOR BY DEFAULT******/
+
+
+/******IMPLEMENTATION OF GETTERS******/
+
+
+/******IMPLEMENTATION OF SETTERS******/
+
+
+
+/******IMPLEMENTATION OF PRIVATE METHODS******/
+
+
+/******IMPLEMENTATION OF PUBLIC METHODS******/
+
+
+
+//------AUXILIARY------
+
+
+//------I/O------
+
+
+
+
+//******OPERATORS OVERLOAD******/
+
+
+
---
+++ HElib/src/CyCtxt.h
@@ -0,0 +1,54 @@
+#ifndef DEF_CYCTXT
+#define DEF_CYCTXT
+
+#include "Cyfhel.h"
+
+//The CyCtxt Class extend the Ctxt Class
+class CyCtxt: public Ctxt {
+
+ private:
+
+    /******ATTRIBUTES******/
+	
+        
+
+
+    /******PROTOTYPES OF PRIVATE METHODS******/
+	
+
+
+ public:
+
+	/******CONSTRUCTOR BY DEFAULT******/
+
+
+	/******CONSTRUCTOR WITH PARAMETERS******/
+	CyCtxt(FHEPubKey const& newPubKey, long newPtxtSpace=0): Ctxt(newPubKey, newPtxtSpace){}//Constructor
+
+
+	/******DESTRUCTOR BY DEFAULT******/
+	
+
+    /******GETTERS******/
+	
+	/******SETTERS******/
+	
+
+       
+    /******PROTOTYPES OF PUBLIC METHODS******/
+
+
+
+	//------AUXILIARY------
+
+    
+	//------I/O------
+
+
+        
+    /******OPERATORS OVERLOAD******/
+
+
+};
+
+#endif

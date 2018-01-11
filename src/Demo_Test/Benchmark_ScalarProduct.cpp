90a52e208843b75a6addc99090d089932d2664f1
remyauda <remyauda@yahoo.fr>
2018-01-11 02:22:44 +0100

    [UPDATE] Write result of benchmark in dedicated files for scalar product.

--- src/Demo_Test/Benchmark_ScalarProduct.cpp
+++ src/Demo_Test/Benchmark_ScalarProduct.cpp
@@ -33,6 +33,7 @@
 
 #include <Cyfhel.h>
 #include "Timer.h"
+#include "LibMatrix.h"
 
 #include <cassert>
 #include <cstdio>
@@ -48,9 +49,14 @@
 /* Define if the polynome is monic or not during the tests.*/
 #define isMonic 0
 
+/* Define the number of execution of Benchmark*/
+#define NB_BENCHMARK 10
+
 
 int main(int argc, char *argv[])
 {
+	vector<double> vectorBenchmark;// Vector for store execution time.
+
 	vector<long> v1; // Initialization of v1.
 	vector<long> v2; // Initialization of v2.
 
@@ -85,21 +91,32 @@ int main(int argc, char *argv[])
     CyCtxt c1 = cy.encrypt(v1);
     CyCtxt c2 = cy.encrypt(v2);
 
-	std::cout <<"******Perform Scalar Product operation******"<<endl<<endl;
+	for(int k=0; k<NB_BENCHMARK; k++)
+	{
+		std::cout <<"******Perform Scalar Product operation "<< k+1 <<"******"<<endl<<endl;
 
-	// Begin the chrono.
-    Timer timerDemo(true);
-    timerDemo.start();
+		// Begin the chrono.
+		Timer timerDemo(true);
+		timerDemo.start();
 
-	//Perform Scalar Product operation
-	// Scalar product with returnScalarProd method of the two cypher text.
-    CyCtxt cScalarProd1_2 = c1.returnScalarProd(c2);
+		//Perform Scalar Product operation
+		// Scalar product with returnScalarProd method of the two cypher text.
+		CyCtxt cScalarProd1_2 = c1.returnScalarProd(c2);
 
-	// Stop the chrono and display the execution time.
-    timerDemo.stop();
-    timerDemo.benchmarkInSeconds();
-    timerDemo.benchmarkInHoursMinutesSecondsMillisecondes(true);
-	timerDemo.benchmarkInYearMonthWeekHourMinSecMilli(true);
+		// Stop the chrono and display the execution time.
+		timerDemo.stop();
+		timerDemo.benchmarkInSeconds();
+		timerDemo.benchmarkInHoursMinutesSecondsMillisecondes(true);
+		timerDemo.benchmarkInYearMonthWeekHourMinSecMilli(true);
+
+		vectorBenchmark.push_back(timerDemo.getm_benchmarkSecond());//Push in the vector the execution time in seconds.
+	}
+
+	double averageOfExecutionTime = std::accumulate( vectorBenchmark.begin(), vectorBenchmark.end(), 0.0)/vectorBenchmark.size();// Compute the average of execution time.
+
+	LibMatrix::writeDoubleInFileWithEraseData("Result_Benchmark_ScalarProduct", averageOfExecutionTime);// Write the double averageOfExecutionTime in the file Result_Benchmark_CreateCyfhel in the directory ResultOfBenchmark.
+
+	LibMatrix::writeStringInFileWithEraseData("ResultVerbose_Benchmark_ScalarProduct", LibMatrix::transformSecondToYearMonthWeekHourMinSecMilli(averageOfExecutionTime));// Write the string verbose to transform the average of execution time in seconds to string verbose Years, Months, Weeks, Hours, Minutes, Seconds, Milliseconds in the file ResultVerbose_Benchmark_CreateCyfhel in the directory ResultOfBenchmark.
 
     // Skip a line.
     std::cout <<"\n"<<endl;

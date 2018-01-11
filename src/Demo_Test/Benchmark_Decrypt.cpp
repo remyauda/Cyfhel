44a2af9343092a0936316f6e142da9164a796c31
remyauda <remyauda@yahoo.fr>
2018-01-11 20:18:44 +0100

    [UPDATE] Write result of benchmark in dedicated files for Decryption operation.

--- src/Demo_Test/Benchmark_Decrypt.cpp
+++ src/Demo_Test/Benchmark_Decrypt.cpp
@@ -33,6 +33,7 @@
 
 #include <Cyfhel.h>
 #include "Timer.h"
+#include "LibMatrix.h"
 
 #include <cassert>
 #include <cstdio>
@@ -48,9 +49,14 @@
 /* Define if the polynome is monic or not during the tests.*/
 #define isMonic 0
 
+/* Define the number of execution of Benchmark*/
+#define NB_BENCHMARK 100
+
 
 int main(int argc, char *argv[])
 {
+	vector<double> vectorBenchmark;// Vector for store execution time.
+
 	vector<long> v1; // Initialization of v1.
 
 	// Initialization of v1.
@@ -68,7 +74,7 @@ int main(int argc, char *argv[])
     // Cyfhel is an object that create keys for homeomorphism encryption with the parameter used in its constructor. 
     // If no parameter are provided, uses default values for the generation of the keys.
     // Cyfhel is an object that allow the user to encrypt and decrypt vectors in a homeomorphism way.
-    std::cout <<"******Generation of the keys for encryption******"<<endl;
+    std::cout <<"******Generation of the keys for encryption******"<<endl<<endl;
 
 	// Use this initialization for strong encryption. However, the computation time will be greater (takes several minutes at least).
 	Cyfhel cy(true);
@@ -76,20 +82,32 @@ int main(int argc, char *argv[])
     // Encrypted the plaintext to have a Cypher text that are encrypted in an homeomorphic way with the key generated during the construction of object Cyfhel. 
     CyCtxt c1 = cy.encrypt(v1);
 
-	std::cout <<"******Homeomorphic decryption******"<<endl;
+	for(int k=0; k<NB_BENCHMARK; k++)
+	{
+		std::cout <<"******Homeomorphic decryption "<< k+1 <<"******"<<endl<<endl;
 
-	// Begin the chrono.
-    Timer timerDemo(true);
-    timerDemo.start();
+		// Begin the chrono.
+		Timer timerDemo(true);
+		timerDemo.start();
 
-	// Decrypt the CyCtxt.
-    vector<long> v1_decrypt = cy.decrypt(c1);
+		// Decrypt the CyCtxt.
+		vector<long> v1_decrypt = cy.decrypt(c1);
 
-	// Stop the chrono and display the execution time.
-    timerDemo.stop();
-    timerDemo.benchmarkInSeconds();
-    timerDemo.benchmarkInHoursMinutesSecondsMillisecondes(true);
-	timerDemo.benchmarkInYearMonthWeekHourMinSecMilli(true);
+		// Stop the chrono and display the execution time.
+		timerDemo.stop();
+		timerDemo.benchmarkInSeconds();
+		timerDemo.benchmarkInHoursMinutesSecondsMillisecondes(true);
+		timerDemo.benchmarkInYearMonthWeekHourMinSecMilli(true);
+
+		vectorBenchmark.push_back(timerDemo.getm_benchmarkSecond());//Push in the vector the execution time in seconds.
+	}
+
+	double averageOfExecutionTime = std::accumulate( vectorBenchmark.begin(), vectorBenchmark.end(), 0.0)/vectorBenchmark.size();// Compute the average of execution time.
+
+	LibMatrix::writeDoubleInFileWithEraseData("Result_Benchmark_Decrypt", averageOfExecutionTime);// Write the double averageOfExecutionTime in the file Result_Benchmark_Decrypt in the directory ResultOfBenchmark.
+
+	LibMatrix::writeStringInFileWithEraseData("ResultVerbose_Benchmark_Decrypt", LibMatrix::transformSecondToYearMonthWeekHourMinSecMilli(averageOfExecutionTime));// Write the string verbose to transform the average of execution time in seconds to string verbose Years, Months, Weeks, Hours, Minutes, Seconds, Milliseconds in the file ResultVerbose_Benchmark_Decrypt in the directory ResultOfBenchmark.
+
 
     // Skip a line.
     std::cout <<"\n"<<endl;

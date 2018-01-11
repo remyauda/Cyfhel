25dd3adef77dd18e8c7985023627df88522086d6
remyauda <remyauda@yahoo.fr>
2018-01-11 18:22:59 +0100

    [UPDATE] Write result of benchmark in dedicated files for Minus operation.

--- src/Demo_Test/Benchmark_Minus.cpp
+++ src/Demo_Test/Benchmark_Minus.cpp
@@ -33,6 +33,7 @@
 
 #include <Cyfhel.h>
 #include "Timer.h"
+#include "LibMatrix.h"
 
 #include <cassert>
 #include <cstdio>
@@ -48,9 +49,14 @@
 /* Define if the polynome is monic or not during the tests.*/
 #define isMonic 0
 
+/* Define the number of execution of Benchmark*/
+#define NB_BENCHMARK 1000
+
 
 int main(int argc, char *argv[])
 {
+    vector<double> vectorBenchmark;// Vector for store execution time.
+
 	vector<long> v1; // Initialization of v1.
 	vector<long> v2; // Initialization of v2.
 
@@ -80,26 +86,39 @@ int main(int argc, char *argv[])
 	// Use this initialization for strong encryption. However, the computation time will be greater (takes several minutes at least).
 	Cyfhel cy(true);
 
+    std::cout <<"******Homeomorphic encryption******"<<endl<<endl;
+
     // Encrypted the two plaintexts to have two Cypher texts that are encrypted in an homeomorphic way with the key generated during the construction of object Cyfhel. 
     // These two Cypher txt will be use for the test on the homeomorphic operation (+=, -=, *=, ...).
     CyCtxt c1 = cy.encrypt(v1);
     CyCtxt c2 = cy.encrypt(v2);
 
-	std::cout <<"******Perform - operation******"<<endl<<endl;
+    for(int k=0; k<NB_BENCHMARK; k++)
+    {
+        std::cout <<"******Perform - operation "<< k+1 <<"******"<<endl<<endl;
 
-	// Begin the chrono.
-    Timer timerDemo(true);
-    timerDemo.start();
+    	// Begin the chrono.
+        Timer timerDemo(true);
+        timerDemo.start();
 
-	//Perform - operation
-	// Substraction of the two cypher text.
-    CyCtxt cSubstract1_2 = c1 - c2;
+    	//Perform - operation
+    	// Substraction of the two cypher text.
+        CyCtxt cSubstract1_2 = c1 - c2;
 
-	// Stop the chrono and display the execution time.
-    timerDemo.stop();
-    timerDemo.benchmarkInSeconds();
-    timerDemo.benchmarkInHoursMinutesSecondsMillisecondes(true);
-	timerDemo.benchmarkInYearMonthWeekHourMinSecMilli(true);
+    	// Stop the chrono and display the execution time.
+        timerDemo.stop();
+        timerDemo.benchmarkInSeconds();
+        timerDemo.benchmarkInHoursMinutesSecondsMillisecondes(true);
+    	timerDemo.benchmarkInYearMonthWeekHourMinSecMilli(true);
+
+        vectorBenchmark.push_back(timerDemo.getm_benchmarkSecond());//Push in the vector the execution time in seconds.
+    }
+
+    double averageOfExecutionTime = std::accumulate( vectorBenchmark.begin(), vectorBenchmark.end(), 0.0)/vectorBenchmark.size();// Compute the average of execution time.
+
+    LibMatrix::writeDoubleInFileWithEraseData("Result_Benchmark_Minus", averageOfExecutionTime);// Write the double averageOfExecutionTime in the file Result_Benchmark_Minus in the directory ResultOfBenchmark.
+
+    LibMatrix::writeStringInFileWithEraseData("ResultVerbose_Benchmark_Minus", LibMatrix::transformSecondToYearMonthWeekHourMinSecMilli(averageOfExecutionTime));// Write the string verbose to transform the average of execution time in seconds to string verbose Years, Months, Weeks, Hours, Minutes, Seconds, Milliseconds in the file ResultVerbose_Benchmark_Minus in the directory ResultOfBenchmark.
 
     // Skip a line.
     std::cout <<"\n"<<endl;
